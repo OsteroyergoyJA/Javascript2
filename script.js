172c49a84126ed72d46743774f4400e1481a775e
OsteroyergoyJA <oosteroyergoy@gmail.com>
2024-11-21 11:56:56 +0100

    Javascript2

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log('Hello!');
+
+//forsøk på å løse logg inn sjekk//
+
+/*
+let fname=any
+let lname=any
+
+function myFunction(fname,lname){
+    const date=new Date();
+    getUTCdate();   
+    date.toLocaleTimeString();string;
+}
+
+*/
+
+let box1=fname
+let box2=lname
+
+function time(_box1,_box2){
+
+const current=new Date();
+current.toDateString(); String;
+document.getElementById("showtime").innerHTML = current;
+
+}
+
+console.log(time)
+
+
+
+//eksempel på forskjellige data//
+
+
